68b1b236b7167db2d9db842834cf4961f0564b4d
mohamedsobhi70 <sobhi7070@gmail.com>
2023-04-11 21:24:41 +0200

    add

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,18 @@
+$('.owl-categories').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:30,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
