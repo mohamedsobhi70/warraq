9f3366c20e5b894595fd81ff81093ecacd5f4096
mohamedsobhi70 <sobhi7070@gmail.com>
2023-04-13 02:01:51 +0200

    add

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,18 +1,45 @@
 $('.owl-categories').owlCarousel({
     rtl: true,
     loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
     margin: 30,
-    nav: true,
     dots: false,
     responsive: {
         0: {
-            items: 2
+            items: 2,
+            nav: false,
         },
-        600: {
+        676: {
             items: 3
         },
-        1000: {
-            items: 6
+        1024: {
+            items: 6,
+            nav: true,
+        }
+    }
+})
+
+$('.owl-bestseller').owlCarousel({
+    rtl: true,
+    autoplay: true,
+    loop: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    margin: 30,
+    nav: false,
+    responsive: {
+        0: {
+            items: 2,
+            dots: true,
+        },
+        768: {
+            items: 3,
+            dots: false,
+        },
+        1024: {
+            items: 4
         }
     }
 })
